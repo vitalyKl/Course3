dccc6ad579923f38f08183ed54e020952f0a1bef
Vitaly Kalinin <vital5991@gmail.com>
2020-09-22 23:49:19 +0300

    Изменил логику работы с сервисами отправки сообщений. Теперь список сервисов находится в текстовом документе (пока не используем БД), а не внутри отдельного класса.

---
+++ Course3/MailService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Course3
+{
+    public class MailService
+    {
+        private string _name;
+        private string _url;
+        private int _port;
+
+        public string Name
+        {
+            get => _name;
+            private set => _name = value;
+        }
+        public string Url
+        {
+            get => _url;
+            private set => _url = value;
+        }
+        public int Port
+        {
+            get => _port;
+            private set => _port = value;
+        }
+
+        Regex reg = new Regex(@"(?<name>\w*)\s(?<url>\w*\.\w*\.\w*)\s(?<port>\d*)", RegexOptions.Compiled);
+
+        public MailService(string input)
+        {
+            var match = reg.Match(input);
+            if (match == null) return;
+            Name = match.Groups["name"].Value;
+            Url = match.Groups["port"].Value;
+            Port = Convert.ToInt32(match.Groups["port"].Value);
+        }
+        
+    }
+}
