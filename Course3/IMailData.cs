3d3475d6caa0ed986bec245f300ba9af68b998d5
Vitaly Kalinin <vital5991@gmail.com>
2020-09-22 23:29:52 +0300

    Сделал интерфейс для получения данныхиз формы отправки сообщения + класс, со списком доступных сервисов по отрпавке сообщений.

---
+++ Course3/IMailData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course3
+{
+    interface IMailData
+    {
+        public string MailSender { get; set; }
+
+        public string MailReciever { get; set; }
+
+        public string MailTitle { get; set; }
+
+        public string MailText { get; set; }
+
+        public string SeviceName { get; set; }
+
+        public string ServiceLogin { get; set; }
+
+        public string ServicePassword { get; set; }
+        
+    }
+}
---
+++ Course3/MailServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course3
+{
+    public static class MailServices
+    {       
+        public static List<string> Services = new List<string>()
+        {
+            "yandex, smtp.yandex.ru, 25",
+            "gmail, smtp.gmail.com, 465",
+            "mail.ru, smtp.mail.ru, 465",
+        };
+    }
+}
