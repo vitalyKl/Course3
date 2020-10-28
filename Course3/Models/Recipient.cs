f6049280e97f689b08fde093e91f5c13db3e20f9
Vitaly Kalinin <vital5991@gmail.com>
2020-10-28 14:13:13 +0300

    Основные модели данных

---
+++ Course3/Models/Recipient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course3.Models
+{
+    class Recipient
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+    }
+}
---
+++ Course3/Models/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course3.Models
+{
+    class Server
+    {
+        private int _Port;
+
+        public string Adress { get; set; }
+        public int Port 
+        { 
+            get => _Port; 
+            set
+            {
+                if (value < 0 || value > 65535)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен быть в диапазоне от 0 до 65535");                    
+                }
+                _Port = value;
+            }
+        }
+        public bool UseSSL { get; set; }
+        public string Description { get; set; }
+    }
+}
